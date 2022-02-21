c7d5aacbb211a629d197f174f7188310153522eb
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-02-22 02:07:27 +0900

    fade in effect

---
+++ PM/Source/Main/LevelSelect.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("fadein")
+
+
+func _on_Erika_pressed():
+	pass # Replace with function body.
+
+
+func _on_Minnie_pressed():
+	pass # Replace with function body.
+
+
+func _on_Hyuna_pressed():
+	pass # Replace with function body.

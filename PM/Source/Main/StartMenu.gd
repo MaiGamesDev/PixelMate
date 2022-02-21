c287bc0e19f2006c02214c1b9b5c31e36a85f655
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-02-21 20:28:05 +0900

    Start Menu (with little animation)

---
+++ PM/Source/Main/StartMenu.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var is_started = false
+
+func go_levelSelect():
+	get_tree().change_scene("res://Source/Main/LevelSelect.tscn")
+
+func _input(event):
+	if event is InputEventKey:
+		if !is_started:
+			$AnimationPlayer.play("start")
+			is_started = true

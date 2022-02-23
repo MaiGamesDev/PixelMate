80ac3295a48cf81e5b1b0f546d13ad62642e589b
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-02-23 17:20:13 +0900

    texture button's "released" texture

--- PM/Source/Main/Button.gd
+++ PM/Source/Main/Button.gd
@@ -1,16 +1,40 @@
 extends TextureButton
 
 export(float) var pressed_space = 10
+export(Texture) var released_texture
 var texture_origin_y = 0
+var label_origin_y = 0
 
 func _ready():
-	texture_origin_y = $Texture.rect_position.y
+	if $Texture != null:
+		texture_origin_y = $Texture.rect_position.y
+	if $Label != null:
+		label_origin_y = $Label.rect_position.y
 	
 	connect("button_down",self,"_on_button_down")
 	connect("button_up",self,"_on_button_up")
 
 func _on_button_down():
-	$Texture.rect_position.y = texture_origin_y + pressed_space
+	if $Texture != null:
+		$Texture.rect_position.y = texture_origin_y + pressed_space
+	if $Label != null:
+		$Label.rect_position.y = label_origin_y + pressed_space
 
 func _on_button_up():
-	$Texture.rect_position.y = texture_origin_y
+	if released_texture != null:
+		self_modulate.a = 0
+		var texture = Sprite.new()
+		texture.texture = released_texture
+		texture.centered = false
+		texture.show_behind_parent = true
+		add_child(texture)
+		
+		yield(get_tree().create_timer(0.15),"timeout")
+		
+		self_modulate.a = 100
+		texture.queue_free()
+
+	if $Texture != null:
+		$Texture.rect_position.y = texture_origin_y
+	if $Label != null:
+		$Label.rect_position.y = label_origin_y

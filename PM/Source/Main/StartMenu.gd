da9600c1726fd06c0c88352db4e840ed292c5022
Anggoro Gagah <aak270@gmail.com>
2022-02-25 14:21:56 +0900

    add fade effects in startmenu

--- PM/Source/Main/StartMenu.gd
+++ PM/Source/Main/StartMenu.gd
@@ -2,11 +2,16 @@ extends Node
 
 var is_started = false
 
+onready var player = $AnimationPlayer
+
+func play_idle():
+	player.play("idle")
+
 func go_levelSelect():
 	get_tree().change_scene("res://Source/Main/LevelSelect.tscn")
 
 func _input(event):
 	if event is InputEventKey or event is InputEventMouseButton:
 		if !is_started:
-			$AnimationPlayer.play("start")
+			player.play("start")
 			is_started = true

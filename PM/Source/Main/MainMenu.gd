03bb854f6e93797a6346860c4140a69314f377f1
Anggoro Gagah <aak270@gmail.com>
2022-02-25 19:15:09 +0900

    girl's profile

--- PM/Source/Main/MainMenu.gd
+++ PM/Source/Main/MainMenu.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func next_scene():
-	get_tree().change_scene("res://Source/Main/StartMenu.tscn")
+	get_tree().change_scene("res://Source/Main/LevelSelect.tscn")
 
 func _on_Start_pressed():
 	$AnimationPlayer.play("fadeout")
